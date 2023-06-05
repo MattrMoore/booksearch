ead45b1539a72cab995e9f79bf0dfd07570e40eb
Matt Moore <mattmoore2014@gmail.com>
2023-06-05 02:07:29 -0600

    organzied the code and added some new files

---
+++ client/src/utils/mutation.js
@@ -0,0 +1,57 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                email
+                password
+        }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                username
+                email
+                password
+            }
+        }
+        }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($bookInfo: BookInput!) {
+        saveBook(bookInfo: $bookInfo) {
+        _id
+        username
+        email
+        savedBooks {
+            bookId
+            authors
+            title
+            description
+            image
+            link
+        }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+        _id
+        username
+        email
+        savedBooks {
+            bookId
+        }
+        }
+    }
+`;
